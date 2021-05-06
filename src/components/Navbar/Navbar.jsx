50d30b1f9ef5df72dfb60a569207603e4ae1cd70
Bijay Tamang <lazon11@outlook.com>
2021-05-06 18:02:17 -0400

    Created Cart Checkout System

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ import { ShoppingCart } from "@material-ui/icons";
 import useStyles from "./styles";
 import logo from "../../assets/commerce.png";
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
   return (
     <AppBar position="fixed" className={classes.appBar} color="inherit">
@@ -29,7 +29,7 @@ const Navbar = () => {
         <div className={classes.grow} />
         <div className={classes.button}>
           <IconButton aria-label="Show cart items" color="inherit">
-            <Badge badgeContent={2} color="secondary">
+            <Badge badgeContent={totalItems} color="secondary">
               <ShoppingCart />
             </Badge>
           </IconButton>
